c2fef56ca5332fbe0cea1682c91337f6c87544bc
Wilfried AGO <wilfriedago@pm.me>
2022-09-21 16:59:58 +0100

    Apply ESlint

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,8 +2,8 @@
 export const getFormattedDate = (date) =>
 	date
 		? new Date(date).toLocaleDateString('en-us', {
-				year: 'numeric',
-				month: 'short',
-				day: 'numeric',
-		  })
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric',
+		})
 		: '';
