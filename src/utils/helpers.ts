7ae86c20607f1a67fa7390c3002ee7e4f516d326
Wilfried AGO <wilfriedago@pm.me>
2022-09-28 19:51:53 +0100

    Simplify the code, by modulation
    Moving correct script in appropriate component

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -7,3 +7,12 @@ export function getStorage(key: string) {
 export function setStorage(key: string, value: unknown) {
 	localStorage.setItem(key, JSON.stringify(value));
 }
+
+export function attachEvent(selector, event, fn) {
+	const matches = document.querySelectorAll(selector);
+
+	if (matches && matches.length)
+		matches.forEach((elem) => {
+			elem.addEventListener(event, () => fn(elem), false);
+		});
+}
